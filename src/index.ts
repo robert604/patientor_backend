3195ea07d60496e2a26374642e4304910bb2e8ce
robert604 <robtemper@gmail.com>
2022-01-24 15:40:28 -0500

    submission for 9.10

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 const cors = require('cors');
+import { Diagnose } from './types';
+import diagnoses from '../data/diagnoses.json';
 
 const app = express();
 app.use(express.json());
@@ -10,6 +12,12 @@ const PORT = 3001;
 app.get('/api/ping',(req,res) => {
   console.log('got a ping');
   res.send('pong');
+})
+
+app.get('/api/diagnoses',(req,res) => {
+  console.log('got req diagnoses');
+  const diags:Diagnose[] = diagnoses
+  res .send(diags)
 })
 
 app.listen(PORT,() => {
