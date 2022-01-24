c66a90075c083ecaee95c7079021f4d3283c3e34
robert604 <robtemper@gmail.com>
2022-01-24 13:49:40 -0500

    submission for 9.9

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
-import express from 'express'
+import express from 'express';
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/ping',(req,res) => {
+app.get('/api/ping',(req,res) => {
   console.log('got a ping');
   res.send('pong');
 })
